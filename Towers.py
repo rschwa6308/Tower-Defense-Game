168252ad89176b2f857e066f0fe10940f193dc70
puzzle_ninja <the.puzzle.ninja@gmail.com>
2017-06-04 16:50:07 -0400

    image and beginning of specific tower types

--- Towers.py
+++ Towers.py
@@ -4,6 +4,7 @@ class Tower:
         self.health_level = 1
         self.damage_level = 1
         self.speed_level = 1
+        self.damage_types = []
 
 
 class Arrow(Tower):
@@ -12,17 +13,23 @@ class Arrow(Tower):
         self.health = 20
         self.damage = 100
         self.speed = 5
+        self.damage_types = ['single']
 
 
-class Mage:
+class Mage(Tower):
     def __init__(self):
         super(Mage, self).__init__()
         self.health = 15
         self.damage = 150
         self.speed = 4
+        self.damage_types = ['splash']
+
+class EarthMage(Mage):
+    def __init__(self):
+        super(EarthMage, self).__init__()
 
 
-class Artillery:
+class Artillery(Tower):
     def __init__(self):
         super(Artillery, self).__init__()
         self.health = 50
