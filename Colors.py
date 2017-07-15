3b06eef0a3557d7fefa573b1860b0118ad98cbb7
Russell Schwartz <russell.w.schwartz@gmail.com>
2017-07-15 16:12:15 -0400

    Background gradient circular option

--- Colors.py
+++ Colors.py
@@ -3,8 +3,12 @@ from pygame import Color
 
 black = Color(0, 0, 0)
 white = Color(255, 255, 255)
+
 red = Color(255, 0, 0)
 green = Color(0, 255, 0)
+blue = Color(0, 0, 255)
+
+yellow = Color(255, 255, 0)
 
 bg_color = Color(0, 0, 150)
 range_color = Color(50, 50, 50)
