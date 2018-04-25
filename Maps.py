18670d7efdc4592731a81226696ec66f5fe6bfc4
zerostarm <zerostarm@users.noreply.github.com>
2018-04-25 12:59:45 -0400

    commented out some print statements

--- Maps.py
+++ Maps.py
@@ -8,11 +8,11 @@ badPixels = []
 
 
 def wrongPixels(mapTk, path_color, _base):
-    print("Bad Pixels")
+    # print("Bad Pixels")
     for i in range(int(screenWidth * widthMultiplier)):
         for j in range(int(screenHeight * heightMultiplier)) : 
             if (mapTk.get_at((i, j)) == path_color or _base.rect.contains(i, j, 0 , 0)):
                 badPixels.append((i, j))
             # print(i, j)
     
-    print("End bad Pixels")
+    # print("End bad Pixels")
